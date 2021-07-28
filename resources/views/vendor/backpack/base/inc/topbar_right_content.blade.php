d0f9cbc82dc2c49972ef599beca1c659257d3eed
Abu Sarya <abukarmo194@gmail.com>
2021-07-28 11:18:18 +0300

    Language menu button

--- resources/views/vendor/backpack/base/inc/topbar_right_content.blade.php
+++ resources/views/vendor/backpack/base/inc/topbar_right_content.blade.php
@@ -1,6 +1,22 @@
 <!-- This file is used to store topbar (right) items -->
 
-
 {{-- <li class="nav-item d-md-down-none"><a class="nav-link" href="#"><i class="la la-bell"></i><span class="badge badge-pill badge-danger">5</span></a></li>
 <li class="nav-item d-md-down-none"><a class="nav-link" href="#"><i class="la la-list"></i></a></li>
 <li class="nav-item d-md-down-none"><a class="nav-link" href="#"><i class="la la-map"></i></a></li> --}}
+
+<li class="nav-item dropdown pr-4">
+    <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+        <img src="{{ url('/images/languages.png') }}" width="25px" height="25px" alt="">
+    </a>
+    <div class="dropdown-menu {{ config('backpack.base.html_direction') == 'rtl' ? 'dropdown-menu-left' : 'dropdown-menu-right' }} mr-4 pb-1 pt-1">
+        <a class="dropdown-item" href="{{ route('backpack.account.info') }}">
+            <img src="{{ url('/images/en_lang.png') }}" width="20px" height="20px" alt="">
+            English
+        </a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="{{ backpack_url('logout') }}">
+            <img src="{{ url('/images/ar_lang.png') }}" width="20px" height="20px" alt="">
+            العربية
+        </a>
+    </div>
+</li>
--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -6,6 +6,8 @@
 // This route file is loaded automatically by Backpack\Base.
 // Routes you generate using Backpack\Generators will be placed here.
 
+use Illuminate\Support\Facades\Route;
+
 Route::group([
     'prefix'     => config('backpack.base.route_prefix', 'admin'),
     'middleware' => array_merge(
@@ -14,4 +16,6 @@ Route::group([
     ),
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
+    Route::get('/set_lang/{lang}/{dir}', function () {
+    });
 }); // this should be the absolute last line of this file
